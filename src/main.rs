6aad2c3348b22f0a508d75795dc5fea964d863c5
Oliver Ellis <ellis.oliver@gmail.com>
2022-04-17 13:38:59 +0200

    Added simple triangle animation

--- src/main.rs
+++ src/main.rs
@@ -39,21 +39,14 @@ fn main() {
         }
     "#;
 
-    // Define vertice
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [0.0, 0.5] };
-    let vertex3 = Vertex { position: [0.5, -0.25] };
-    let shape = vec![vertex1, vertex2, vertex3];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
+    
     // 5. Run event_loop infinitely
+    let mut t: f32 = -0.5;
     event_loop.run(move |event, _, control_flow| {
-        let next_frame_time = Instant::now() + Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-
+        
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
@@ -69,7 +62,23 @@ fn main() {
             },
             _ => return,
         }
-
+        
+        let next_frame_time = Instant::now() + Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        
+        // Update t
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+        
+        // Define vertice
+        let vertex1 = Vertex { position: [-0.5 + t, -0.5] };
+        let vertex2 = Vertex { position: [ 0.0 + t,  0.5] };
+        let vertex3 = Vertex { position: [ 0.5 + t, -0.25] };
+        let shape = vec![vertex1, vertex2, vertex3];
+        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+        
         // Draw triangle
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
